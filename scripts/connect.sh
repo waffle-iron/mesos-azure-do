c709e490227843b1cab343e3cbfc27939ec94449
Daniel Scholl <degnome@gmail.com>
2017-02-03 09:38:47 -0600

    Initial Code

---
+++ scripts/connect.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#set -o errexit -o pipefail
+
+ENV_SETUP=".env"
+source "$ENV_SETUP"
+
+#////////////////////////////////
+echo 'Connecting to' $HOST
+echo ''
+echo 'You can connect to DCOS at http://localhost:8080'
+echo''
+echo 'You can connect to Mesos at http://localhost:8080/mesos/'
+echo ''
+echo 'You can connect to Marathon at http://localhost:8080/marathon'
+echo ''
+ssh -i ./ssh/id_rsa $USER@$MASTER0_FQDN -A -p 2200 -L 8080:localhost:80
---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#set -o errexit -o pipefail
+
+ENV_SETUP=".env"
+source "$ENV_SETUP"
+
+azure login
+azure account set ${AZURE_SUBSCRIPTION}
+azure config mode arm
+azure group create --location ${AZURE_LOCATION} --name ${AZURE_RESOURCE_GROUP}
+azure group deployment create --template-uri https://raw.githubusercontent.com/azure/azure-quickstart-templates/master/101-acs-dcos/azuredeploy.json --parameters-file templates/params.json ${AZURE_RESOURCE_GROUP} dcosDeploy
