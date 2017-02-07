dfff911b0397e5211f0d8ca22431579a95caa820
Daniel Scholl <degnome@gmail.com>
2017-02-07 13:59:19 -0600

    InsecureRegistry script

--- tools/registry.sh
+++ tools/registry.sh
@@ -6,13 +6,14 @@ source "$ENV_SETUP"
 
 tput setaf 1; echo 'This will enable the Docker daemons in the cluster to connect to registry.marathon.mesos:5000'
 
-# Distribute the addInsecureRegistry.sh script to all nodes in the cluster.
+# Distribute the insecureRegistry.sh script to all nodes in the cluster.
+IFS=$'\n'
 FILE1=agentlist
 hosts1=`cat $FILE1`
 
 for line in $hosts1;
 do
-  /usr/bin/scp addInsecureRegistry.sh $line:~
+  /usr/bin/scp insecureRegistry.sh $line:~
 done
 
-pdsh -R ssh -l $CLUSTER_USERNAME -w  ^agentlist  "sudo addInsecureRegistry.sh"
+pdsh -R ssh -l $CLUSTER_USERNAME -w  ^agentlist  "sudo chmod 744 insecureRegistry.sh && sudo ./insecureRegistry.sh"
